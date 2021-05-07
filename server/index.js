26b6d1a2108b1c5cce042317f84e88f38b3aad05
AyujPanchal <b19099@students.iitmandi.ac.in>
2021-05-07 11:36:25 +0530

    deploy to heroku and netlify

--- server/index.js
+++ server/index.js
@@ -14,8 +14,11 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Hello to memories API');
+});
 
-// const CONNECTION_URL = 'mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.hpavh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.hpavh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
